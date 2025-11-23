7e7a739e7d3bc81f560f1a4f861368a8e285040a
zaorsha <zaorsha@gmail.com>
2025-11-23 14:50:18 +0000

    limiting fps to 60

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game and create game resources."""
         pygame.init()
+        self.clock = pygame.time.Clock()
         
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
@@ -19,6 +20,7 @@ class AlienInvasion:
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)  # Limit the frame rate to 60 FPS
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
