7bb8150743f0a41128920e8123713b4434a80ea6
zaorsha <zaorsha@gmail.com>
2025-11-23 15:18:30 +0000

    ship added to screen. Code refactored

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,26 +1,42 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
         """Initialize the game and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
-            self.clock.tick(60)  # Limit the frame rate to 60 FPS
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+        self.clock.tick(60)  # Limit the frame rate to 60 FPS
+         
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
